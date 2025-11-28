cbf3c1215077555d0d6739090593b6aa662ee4e1
lisstig <lisstig71@gmail.com>
2025-11-28 15:44:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 st.set_page_config(page_title="Karbo-Robot", page_icon="🍖")
 
 # --- DIN API NØKKEL ---
-API_KEY = "aiKUN2TjEqyHrZl8kKNgzbEKnbY3HqREOIEPVetQ"
+API_KEY = "x2Y4R0b7NwDZpB19DRlljFlUFQmaT9aMgbzOrN8L"
 
 # --- INITIALISER HUKOMMELSE ---
 if 'kurv' not in st.session_state:
