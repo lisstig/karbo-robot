41483ac7e4c63b78916b399b0e7635ed011eee13
lisstig <lisstig71@gmail.com>
2025-12-06 00:18:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Karbo-Robot", page_icon="🍖")
 
 # --- DIN API NØKKEL ---
 # HUSK: Bytt ut teksten under med din nye nøkkel fra Kassalapp.no!
-API_KEY = x2Y4R0b7NwDZpB19DRlljFlUFQmaT9aMgbzOrN8L
+API_KEY = "x2Y4R0b7NwDZpB19DRlljFlUFQmaT9aMgbzOrN8L"
 
 # --- INITIALISER HUKOMMELSE ---
 if 'kurv' not in st.session_state:
